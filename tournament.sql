eb93b1594b72222d4fc97202717793e282811843
Ordanis <ordanisanchez@gmail.com>
2017-01-24 14:17:20 -0400

    Add tables schemas and views to tournament sql

--- tournament.sql
+++ tournament.sql
@@ -7,3 +7,21 @@
 -- these lines here.
 
 
+CREATE TABLE players ( name VARCHAR(50),
+                       id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE matches ( win INT REFERENCES players(id),
+                       loss INT REFERENCES players(id),
+                       id SERIAL PRIMARY KEY
+);
+
+CREATE VIEW wins AS 
+            select players.id, count(matches.win) as wins 
+            from players left  join matches on players.id = matches.win 
+            group by players.id;
+
+CREATE VIEW matches_played AS
+            select players.id, count(matches) as matches 
+            from players left  join matches on players.id = matches.win or players.id = matches.loss 
+            group by players.id;
